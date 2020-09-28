401f50f0ba6a9beed33a239a88b9cf7ffe18a088
Suraj Dalvi <63668801+Suraj-dev27@users.noreply.github.com>
2020-09-29 00:47:34 +0530

    Update stone paper scissor.py
    removed User_hoose() function

--- stone paper scissor.py
+++ stone paper scissor.py
@@ -2,8 +2,8 @@ import random
 user_wins = 0
 comp_wins = 0
 
-def User_Choose():
-    user_choice = input("choose Rock, Scissors, Paper: ")
+'''def User_Choose():
+    user_choice = input("choose Rock, Paper, Scissors: ")
     if user_choice in ["rock", "Rock","r","R"]:
         user_choice = "r"
     elif user_choice in ["paper","Paper","p","P"]:
@@ -11,9 +11,9 @@ def User_Choose():
     elif user_choice in ["scissors","Scissors","s","S"]:
         user_choice = "s"
     else:
-        print("I don't understand, try again")
+        print("I don't understand, please try again")
         User_Choose()
-    return user_choice
+    return user_choice'''
 
 def Comp_Choose():
     comp_choice = random.randint(1,3)
@@ -21,15 +21,16 @@ def Comp_Choose():
         comp_choice="r"
     elif comp_choice==2:
         comp_choice="p"
-    else:
+    elif comp_choice==3:
         comp_choice="s"
     return comp_choice
 
-
-user_choice = User_Choose()
+#user_choice = User_Choose()
 comp_choice = Comp_Choose()
 
-if user_choice=="r":
+user_choice=input("choose Rock, Paper or Scissors:")
+
+if user_choice in ["rock", "Rock","r","R"]:
         if comp_choice=="r":
             print("user selected Rock and computer selected Rock, match tied!!")
         elif comp_choice=="p":
@@ -38,8 +39,8 @@ if user_choice=="r":
         elif comp_choice=="s":
             print("user selected Rock and computer selected Scissors, User Wins!!")
             user_wins+=1
-            
-elif user_choice=="p":
+
+elif user_choice in ["paper","Paper","p","P"]:
         if comp_choice=="r":
             print("user selected Paper and computer selected Rock, User Wins!!")
             user_wins+=1
@@ -49,7 +50,7 @@ elif user_choice=="p":
             print("user selected Paper and computer selected Scissors, Computer Wins!!")
             comp_wins+=1
 
-elif user_choice=="s":
+elif user_choice in ["scissors","Scissors","s","S"]:
         if comp_choice=="r":
             print("user selected Scissors and computer selected Rock, Computer Wins!!")
             comp_wins+=1
@@ -63,11 +64,15 @@ print("User Wins: "+ str(user_wins))
 print("Computer Wins: "+ str(comp_wins))
 
 user_choice=input("Do you want to play again (y/n): ")
-if user_choice in ["yes","Yes","y","Y"]:
-    User_hoose()
-elif user_choice in["no","No","n","N"]:
+if user_choice in["yes","Yes","y","Y"]:
+    print("Let's play again!!")
+    Comp_Choose()
+elif user_choice in ["no","No","n","N"]:
     if user_wins > comp_wins:
-        print("User won the game!!")
+        print("User won the game by",user_wins,"-",comp_wins)
     else:
-        print("Computer won the game!!")
-print("Game has ended")
+        print("Computer won the game by",comp_wins,"-",user_wins)
+    print("Game has ended!!")
+
+
+    
